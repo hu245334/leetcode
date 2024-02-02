e5dfc325a11ead9ac4331fdc4bfba4efb3262214
Become Who <18795451009@163.com>
2024-02-02 11:14:41 +0800

    2024 week5

---
+++ l206.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        last = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return last
