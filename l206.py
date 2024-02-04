4ce5d7da592949708c9889531a099518cd116e24
Become Who <18795451009@163.com>
2024-02-04 09:45:42 +0800

    2024 week5

--- l206.py
+++ l206.py
@@ -9,9 +9,11 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 递归终止条件
         if head is None or head.next is None:
             return head
         last = self.reverseList(head.next)
+        # reverse之后的最后一个的next指向head
         head.next.next = head
         head.next = None
         return last
